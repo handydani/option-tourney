3908074deadfdfb41de076799f2426feb0d2c9ef
Daniela Isabel Travieso <dtravie@gmail.com>
2017-01-26 11:20:33 -0500

    Added the java file

---
+++ optionTourney.java
@@ -0,0 +1,18 @@
+//option tourney
+import java.util.*;
+public class optionTourney 
+{
+	public static void main(String[] args) 
+	{
+		Scanner in = new Scanner(System.in);
+
+	}
+
+	//picture generator
+
+	//option object
+
+
+
+
+}
